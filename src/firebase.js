89e033649a15aa66cc8f950d5f25751dab288109
Tulsi Prasad <tulsi.prasad50@gmail.com>
2022-06-17 20:45:54 +0530

    Setup firebase in react

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfwFELXR4az0v24V8h5JaFjwhal34cWn0",
+  authDomain: "test-firebase-9-85e62.firebaseapp.com",
+  projectId: "test-firebase-9-85e62",
+  storageBucket: "test-firebase-9-85e62.appspot.com",
+  messagingSenderId: "565868376774",
+  appId: "1:565868376774:web:d8dc6a80e837b83145ddc7"
+};
+
+// init
+initializeApp(firebaseConfig);
+
+// services
+// collection
+// get collection
